20287bd007534a4b8f80b6cd7c672d2a57860dad
GuruOfPython <vpeKJmPc@mail.com>
2019-01-03 02:32:18 -0800

    Initial Commit

---
+++ Read_XLSX/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.read_xlsx_button = new System.Windows.Forms.Button();
+            this.http_request_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // read_xlsx_button
+            // 
+            this.read_xlsx_button.Location = new System.Drawing.Point(50, 12);
+            this.read_xlsx_button.Name = "read_xlsx_button";
+            this.read_xlsx_button.Size = new System.Drawing.Size(84, 48);
+            this.read_xlsx_button.TabIndex = 0;
+            this.read_xlsx_button.Text = "Read XLSX File";
+            this.read_xlsx_button.UseVisualStyleBackColor = true;
+            this.read_xlsx_button.Click += new System.EventHandler(this.read_xlsx_button_Click);
+            // 
+            // http_request_button
+            // 
+            this.http_request_button.Location = new System.Drawing.Point(186, 18);
+            this.http_request_button.Name = "http_request_button";
+            this.http_request_button.Size = new System.Drawing.Size(107, 37);
+            this.http_request_button.TabIndex = 1;
+            this.http_request_button.Text = "Http Request";
+            this.http_request_button.UseVisualStyleBackColor = true;
+            this.http_request_button.Click += new System.EventHandler(this.http_request_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 342);
+            this.Controls.Add(this.http_request_button);
+            this.Controls.Add(this.read_xlsx_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button read_xlsx_button;
+        private System.Windows.Forms.Button http_request_button;
+    }
+}
+
---
+++ Read_XLSX/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void read_xlsx_button_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+
+            string str;
+            int rCnt;
+            int cCnt;
+            int rw = 0;
+            int cl = 0;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(@"E:\9_Github\12_GuruOfPython\Read-Excel-File-in-C-\Read_XLSX\HE+D4D-Mailing List.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;
+            cl = range.Columns.Count;
+
+
+            for (rCnt = 1; rCnt <= rw; rCnt++)
+            {
+                List<string> row = new List<string>();
+                for (cCnt = 1; cCnt <= cl; cCnt++)
+                {
+                    //str = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
+                    //MessageBox.Show(str);
+                    try
+                    {
+                        str = (range.Cells[rCnt, cCnt] as Excel.Range).Value2.ToString();
+                    }
+                    catch
+                    {
+                        str = "";
+                    }
+                    row.Add(str);
+                }
+                string row_str = string.Join(",", row.ToArray());
+                Console.WriteLine(row_str);
+            }
+
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        private void http_request_button_Click(object sender, EventArgs e)
+        {
+            var responseString = await client.GetStringAsync("http://www.example.com/recepticle.aspx");
+            Console.WriteLine(responseString);
+        }
+    }
+}
